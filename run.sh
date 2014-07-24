25db3cb87e5d1fc0f8bb87ddeb5797512b6b4bd4
Sridhar Sarnobat <ss401533@gmail.com>
2014-07-23 20:22:14 -0700

    Minor

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-script.groovy --block-size=10
+cat lorem_ipsum.txt | script.groovy command --block-size=10000 command2
+
+script.groovy commandA --block-size=9999 
--- script.groovy
+++ script.groovy
@@ -14,35 +14,33 @@ import org.apache.commons.cli.Parser;
 
 public class OptionsParser {
 	public static void main(String[] args) throws IOException {
-		{		
-			BufferedReader sin = new BufferedReader(new InputStreamReader(System.in));
+		
+		BufferedReader sin = new BufferedReader(new InputStreamReader(System.in));
+		while (sin.ready()) {
 			System.out.println(sin.readLine());
 		}
-
 		// create the command line parser
 		Parser parser = new GnuParser();
 
 		// create the Options
 		Options options = new Options();
-		{
-			options.addOption("a", "all", false,
-					"do not hide entries starting with .");
-			options.addOption("A", "almost-all", false,
-					"do not list implied . and ..");
-			options.addOption("b", "escape", false,
-					"print octal escapes for nongraphic " + "characters");
-			options.addOption(OptionBuilder.withLongOpt("block-size")
-					.withDescription("use SIZE-byte blocks").hasArg()
-					.withArgName("SIZE").create());
-			options.addOption("B", "ignore-backups", false,
-					"do not list implied entried " + "ending with ~");
-			options.addOption("c", false,
-					"with -lt: sort by, and show, ctime (time of last "
-							+ "modification of file status information) with "
-							+ "-l:show ctime and sort by name otherwise: sort "
-							+ "by ctime");
-			options.addOption("C", false, "list entries by columns");
-		}
+		options.addOption("a", "all", false,
+				"do not hide entries starting with .");
+		options.addOption("A", "almost-all", false,
+				"do not list implied . and ..");
+		options.addOption("b", "escape", false,
+				"print octal escapes for nongraphic " + "characters");
+		options.addOption(OptionBuilder.withLongOpt("block-size")
+				.withDescription("use SIZE-byte blocks").hasArg()
+				.withArgName("SIZE").create());
+		options.addOption("B", "ignore-backups", false,
+				"do not list implied entried " + "ending with ~");
+		options.addOption("c", false,
+				"with -lt: sort by, and show, ctime (time of last "
+						+ "modification of file status information) with "
+						+ "-l:show ctime and sort by name otherwise: sort "
+						+ "by ctime");
+		options.addOption("C", false, "list entries by columns");
 
 		// This comes from run.sh
 		//args = new String[1];
